78205f04c0b9d07e72b7ed063cefae7bbc734934
Alex Tatulchenkov <webtota@gmail.com>
2014-08-12 00:45:39 +0300

    Examples with monolog

--- examples/client.php
+++ examples/client.php
@@ -1,7 +1,17 @@
 <?php
+use Monolog\Formatter\HtmlFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$client = new \Zoya\Gearman\Client(new Monolog\Logger('CLIENT'));
+
+$stream = new StreamHandler('php://stderr', Logger::DEBUG);
+$formatter = new HtmlFormatter();
+$stream->setFormatter($formatter);
+$logger = new \Monolog\Logger('CLIENT');
+$logger->pushHandler($stream);
+$client = new \Zoya\Gearman\Client($logger);
 for ($i=0; $i<5;$i++) {
     $client->addTaskToWorker('test-valera', ['name'=>'valera', 'number'=>$i]);
 }
